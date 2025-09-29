54864a23f28e291da772d8a2b0ed3d764247f650
Ferraz Junior, N. <norisjunior@gmail.com>
2025-09-29 15:06:07 -0300

    acesso à página interna

--- aulas/aula18_ssrf_csrf_waf_ratelimiting/RATE_LIMIT/rate_streamlit_protegido.py
+++ aulas/aula18_ssrf_csrf_waf_ratelimiting/RATE_LIMIT/rate_streamlit_protegido.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import time
+import requests
 
 st.title("Com rate limiting")
+
+url = "http://127.0.0.1:5000"
+status_area = st.empty()
 
 now = time.time()
 visitas = st.session_state.get("visitas", [])
@@ -14,3 +18,16 @@ else:
         visitas.append(now)
         st.session_state["visitas"] = visitas
         st.success("Executado!")
+
+        status_area = st.empty()
+        texto = ""
+        for i in range(0,5):
+            time.sleep(0.25)
+            texto += "."
+            status_area.info(texto)
+        
+        st.success(f"Executado! Visitas = {len(visitas)}")
+        r = requests.get(url)
+        st.code(r.text)
+
+
--- aulas/aula18_ssrf_csrf_waf_ratelimiting/RATE_LIMIT/rate_streamlit_vuln.py
+++ aulas/aula18_ssrf_csrf_waf_ratelimiting/RATE_LIMIT/rate_streamlit_vuln.py
@@ -1,12 +1,27 @@
 # streamlit_rate_vuln.py
 import streamlit as st
+import requests
+import time
 
 st.title("Rate Limiting Demo - Vulnerável")
+
+url = "http://127.0.0.1:5000"
+status_area = st.empty()
 
 # inicializa contador na sessão
 if "contador" not in st.session_state:
     st.session_state.contador = 0
 
 if st.button("Executar"):
+    status_area = st.empty()
+    texto = ""
+    for i in range(0,5):
+        time.sleep(0.5)
+        texto += "."
+        status_area.info(texto)
+    
     st.session_state.contador += 1
     st.success(f"Executado! Contador = {st.session_state.contador}")
+    r = requests.get(url)
+    st.code(r.text)
+
